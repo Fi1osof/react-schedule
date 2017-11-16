d4184496ed651d17bf7fde7863f8e4db8705d5cf
Fi1osof <n.lanets@modxclub.ru>
2017-11-16 13:41:21 +0300

    1.1.0

--- src/index.js
+++ src/index.js
@@ -23,9 +23,9 @@ import 'react-big-calendar/lib/addons/dragAndDrop/styles.less';
 import moment from 'moment';
 import locale from 'moment/src/locale/ru';
 
-// if(typeof window !== "undefined"){
-// 	window.moment = moment;
-// }
+if(typeof window !== "undefined"){
+	window.moment = moment;
+}
 
 // console.log('messages', messages);
 
@@ -39,7 +39,60 @@ BigCalendar.setLocalizer(
 const DragAndDropCalendar = withDragAndDrop(BigCalendar);
 
 let allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k]);
-allViews = allViews.filter(n => n === "week");
+// allViews = allViews.filter(n => n === "week");
+
+
+
+
+export const objectToDate = function(object, toArray){
+
+	let {
+		// day,
+		start,
+		end,
+	} = object;
+
+	object = Object.assign({}, object, {
+		start: toDate(start, toArray),
+		end: toDate(end, toArray),
+	});
+
+	return object;
+
+};
+
+
+export const toDate = function(array, toArray){
+
+	if(!array){
+		return null;
+	}
+
+	let {
+		year,
+		month,
+		day,
+		hour,
+		minute,
+		second,
+		weekDay,
+	} = array;
+
+	let date = moment();
+
+	date.set("day", weekDay + 1);
+	date.set("hour", hour);
+	date.set("minute", minute);
+	date.set("second", second);
+
+	if(toArray){
+		date = date.toDate();
+	}
+
+	return date;
+
+}
+
 
 export class ReactSchedule extends Component{
 
@@ -71,13 +124,47 @@ export class ReactSchedule extends Component{
 	}
 
 
-	// onSelect(a,b,c){
+	onSelect(event,b,c){
 
-	// 	console.log("onSelect a", a);
-	// 	console.log("onSelect b", b);
-	// 	console.log("onSelect c", c);
 
-	// }
+		if(window.confirm("Удалить этот элемент?")){
+
+			// console.log("onSelect event", event);
+			// console.log("onSelect b", b);
+			// console.log("onSelect c", c);
+
+			let {
+				days,
+			} = this.state;
+
+			const {
+				start,
+			} = event;
+
+			if(!start){
+				return false;
+			}
+
+			const day = moment(start).weekday();
+
+
+			// const index = days && days.indexOf(event);
+
+			// console.log("event index", index);
+
+			// console.log("event days", days);
+
+			days[day] = null;
+
+			// this.setState({
+			// 	days,
+			// });
+
+			this.onChange(days);
+
+		}
+
+	}
 
 	onSelectSlot(slotInfo){
 		// console.log(
@@ -101,53 +188,101 @@ export class ReactSchedule extends Component{
 		start = moment(start);
 		end = moment(end);
 
-		const dayIndex = start.weekday();
+		let dayIndex = start.weekday();
 
-		let prevDay = days && days[dayIndex];
+		let prevDate = days && days[dayIndex];
+
+		let prevDateObject = prevDate && objectToDate(prevDate);
+
+		let yesderday;
 
 		// console.log('onSelectSlot start', start, moment(start).weekday());
 
 
-		let startHour = start.get('hour');
-		let startMinute = start.get('minute');
+		// let startHour = start.get('hour');
+		// let startMinute = start.get('minute');
 
-		let endHour = end.get('hour');
-		let endMinute = end.get('minute');
+		// let endHour = end.get('hour');
+		// let endMinute = end.get('minute');
 
 		// console.log("newEvent", newEvent);
 
-		// Получаем предыдущую информацию за этот день, если она была
+		// Получаем предыдущую информацию за этот день, если она была,
+		// то добавляем время к предыдущему таймеру
 
 		switch(action){
 
 			case 'click':
 
-				if(prevDay){
+				if(
+					!prevDate
+					|| (prevDateObject && prevDateObject.start && prevDateObject.start > start)
+				){
 
-					const {
+					yesderday = days && days[dayIndex - 1];
+
+					if(yesderday){
+
+						prevDate = yesderday;
+
+					}
+				}
+
+				if(prevDate){
+
+					// console.log('prevDate', prevDate);
+
+					prevDate = objectToDate(prevDate);
+
+					// console.log('prevDate', prevDate);
+
+					let {
 						start: prevStart,
 						end: prevEnd,
-					} = prevDay;
+					} = prevDate;
 
-					const {
-						hour: prevStartHour,
-						minute: prevStartMinute,
-					} = prevStart || {};
+					// Если предыдущий день 
+					if(
+						start > prevEnd
+						// Если длительность не более суток
+						&& (end - prevStart) < (24*60*60*1000)
+						// Если не отметка за сутки
+						&& !(start.get("hour") === end.get("hour") && start.get("minute") === end.get("minute"))
+					){
+						start = prevStart;
 
-					const {
-						hour: prevEndHour,
-						minute: prevEndMinute,
-					} = prevEnd || {};
+						if(yesderday){
 
-					
-					if(prevStartHour !== undefined){
+							dayIndex--;
 
-						if(prevStartHour * 60 + prevStartMinute < startHour * 60 + prevEndMinute){
-							startHour = prevStartHour;
-							startMinute = prevStartMinute;
 						}
 
 					}
+
+					// const {
+					// 	start: prevStart,
+					// 	end: prevEnd,
+					// } = prevDate;
+
+					// const {
+					// 	hour: prevStartHour,
+					// 	minute: prevStartMinute,
+					// } = prevStart || {};
+
+					// const {
+					// 	hour: prevEndHour,
+					// 	minute: prevEndMinute,
+					// } = prevEnd || {};
+
+					
+					// if(prevStartHour !== undefined){
+
+					// 	if(prevStartHour * 60 + prevStartMinute < startHour * 60 + prevEndMinute){
+					// 		startHour = prevStartHour;
+					// 		startMinute = prevStartMinute;
+					// 	}
+
+					// }
 
 				}
 
@@ -156,15 +291,60 @@ export class ReactSchedule extends Component{
 		}
 
 
-		let newStart = {
-			hour: startHour,
-			minute: startMinute,
-		};
 
-		let newEnd = {
-			hour: endHour,
-			minute: endMinute,
-		};
+		// let startArray = start && start.toArray();
+
+		let newStart;
+
+		if(start){
+
+			let {
+				0: year, 
+				1: month, 
+				2: day,
+				3: hour,
+				4: minute,
+				5: second,
+			} = {...start.toArray()}
+
+			newStart = {
+				year,
+				month,
+				day,
+				hour,
+				minute,
+				second,
+				weekDay: start.weekday(),
+			};
+
+		}
+
+
+		let newEnd;
+
+
+		if(end){
+
+			let {
+				0: year, 
+				1: month, 
+				2: day,
+				3: hour,
+				4: minute,
+				5: second,
+			} = {...end.toArray()};
+
+			newEnd = {
+				year,
+				month,
+				day,
+				hour,
+				minute,
+				second,
+				weekDay: end.weekday(),
+			};
+
+		}
 
 
 		let newEvent = {
@@ -180,6 +360,7 @@ export class ReactSchedule extends Component{
 		this.onChange(days);
 
 	}
+
 
 	onChange(days){
 		
@@ -256,42 +437,59 @@ export class ReactSchedule extends Component{
 				return;
 			}
 
-			const {
-				start,
-				end,
-			} = n;
+			// const {
+			// 	start,
+			// 	end,
+			// } = n;
 
-			if(!start || !end){
-				return;
-			}
+			// if(!start || !end){
+			// 	return;
+			// }
 
-			const {
-				hour: startHour,
-				minute: startMinute,
-			} = start;
+			// const {
+			// 	hour: startHour,
+			// 	minute: startMinute,
+			// } = start;
 
-			const {
-				hour: endHour,
-				minute: endMinute,
-			} = end;
+			// const {
+			// 	hour: endHour,
+			// 	minute: endMinute,
+			// } = end;
 
-			// console.log("days n", n);
+			// // console.log("days n", n);
 
-			let newData = startDay.clone().add('day', index);
+			// let newData = startDay.clone().add('day', index);
+			// let newStart = newData.clone().add('hour', startHour).add('minute', startMinute);
+			// let newEnd = newData.clone().add('hour', endHour).add('minute', endMinute);
 
-			let newStart = newData.clone().add('hour', startHour).add('minute', startMinute).toDate();
-			let newEnd = newData.clone().add('hour', endHour).add('minute', endMinute).toDate();
+			// // newStart = newStart.toArray();
+			// // newStart[6] = 0;
 
-			let event = {
-				start: newStart,
-				end: newEnd,
-			};
+			// // newEnd = newEnd.toArray();
+			// // newEnd[6] = 0;
+
+			// // let event = {
+			// // 	start: new Date(newStart),
+			// // 	end: new Date(newEnd),
+			// // };
+
+			// let event = {
+			// 	start: newStart.toDate(),
+			// 	end: newEnd.toDate(),
+			// 	allDay: false,
+			// };
+
+			// console.log('event', n);
+
+			let event = objectToDate(n, true);
 
 			events.push(event);
 
 			// console.log('event', event, moment(newStart).toArray(), moment(newEnd).toArray());
 
 		});
+
+		// console.log('events', events);
 
 		// events.push({
 		// 	title: "SDfsdf",
@@ -307,26 +505,28 @@ export class ReactSchedule extends Component{
 			}}
 		>
       <DragAndDropCalendar
-        {...other}
         selectable
         toolbar={false}
         events={events}
         views={allViews}
-        view="week"
+        defaultView="week"
+        // view="month"
         step={30}
         defaultDate={currentDate}
         culture={'ru'}
-        // onSelectEvent={::this.onSelect}
+        onSelectEvent={::this.onSelect}
         onSelectSlot={::this.onSelectSlot}
         onEventDrop={::this.moveEvent}
         // allDayAccessor="Весь день"
         messages={messages}
+        showMultiDayTimes={true}
         // onDrillDown={() => {
         // 	console.log("onDrillDown");
         // }}
         formats={{
         	dayFormat: "dddd",
         }}
+        {...other}
       />
 		</div>
 	}
